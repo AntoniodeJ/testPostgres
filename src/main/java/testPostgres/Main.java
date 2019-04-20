2e9100f7607f094d4adbad2277ad18cfdfbbba9f
AntoniodeJ <antonio_jesus_10@hotmail.com>
2019-04-20 02:17:51 -0300

    urrah!

--- src/main/java/testPostgres/Main.java
+++ src/main/java/testPostgres/Main.java
@@ -5,13 +5,16 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		try {
 			Connection conn = getConnection();
-			String insertTableSQL = "CREATE TABLE treinamento";
+			Scanner entrada = new Scanner(System.in);
+			String string = entrada.nextLine();
+			String insertTableSQL = string;
 			PreparedStatement preparedStatement = conn.prepareStatement(insertTableSQL);
 			preparedStatement.executeQuery();
 			conn.close();
